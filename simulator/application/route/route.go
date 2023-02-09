e790002e9c6f5fed23829525f832b4a8984886e2
Salmo Mascarenhas <salmo.cruz@gmail.com>
2023-02-09 15:11:00 -0300

    route methods.

--- simulator/application/route/route.go
+++ simulator/application/route/route.go
@@ -1,82 +1,87 @@
 package route
 
+import (
+	"bufio"
+	"encoding/json"
+	"errors"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// Route represents a request of new delivery request
 type Route struct {
-	ID string `json: "routeId"`
-	ClientID string `json: "clientId"`
-	Positions []Position `json: "positions"`
+	ID        string     `json:"routeId"`
+	ClientID  string     `json:"clientId"`
+	Positions []Position `json:"position"`
 }
 
+// Position is a type which contains the lat and long
 type Position struct {
-	Latitude float64 `json: "latitude"`
-	Longitude float64 `json: "longitude"`
+	Lat  float64 `json:"lat"`
+	Long float64 `json:"long"`
 }
 
+// PartialRoutePosition is the actual response which the system will return
 type PartialRoutePosition struct {
-	ID string `json:"routeId"`
-	ClientID string `json:"clientId"`
+	ID       string    `json:"routeId"`
+	ClientID string    `json:"clientId"`
 	Position []float64 `json:"position"`
-	Finished bool `json:"finished"`
+	Finished bool      `json:"finished"`
 }
 
-func(r *Route) LoadPositions() error {
-	if r.ID == "" {
-		return errors.New("Route ID is required")
-	}
+// NewRoute creates a *Route struct
+func NewRoute() *Route {
+	return &Route{}
+}
 
+// LoadPositions loads from a .txt file all positions (lat and long) to the Position attribute of the struct
+func (r *Route) LoadPositions() error {
+	if r.ID == "" {
+		return errors.New("route id not informed")
+	}
 	f, err := os.Open("destinations/" + r.ID + ".txt")
 	if err != nil {
 		return err
 	}
-
 	defer f.Close()
-
-	scanner := _bufio.NewScanner(f)
-
+	scanner := bufio.NewScanner(f)
 	for scanner.Scan() {
 		data := strings.Split(scanner.Text(), ",")
-		
-		lat, err := strconv.ParseFloat(data[0], 64)
+		lat, err := strconv.ParseFloat(data[1], 64)
 		if err != nil {
-			return err
+			return nil
 		}
-
-		long, err := strconv.ParseFloat(data[1], 64)
+		long, err := strconv.ParseFloat(data[0], 64)
 		if err != nil {
-			return err
+			return nil
 		}
-
 		r.Positions = append(r.Positions, Position{
-			Latitude: lat, 
-			Longitude: long
+			Lat:  lat,
+			Long: long,
 		})
 	}
-
 	return nil
-
 }
 
-func(r *Route) ExportJasonPositions() ([]byte, error) {
+// ExportJsonPositions generates a slice of string in Json using PartialRoutePosition struct
+func (r *Route) ExportJsonPositions() ([]string, error) {
 	var route PartialRoutePosition
 	var result []string
-	total := leng(r.Positions)
-
-	for k,v := range r.Positions {
-		route.ID = r.ClientID
+	total := len(r.Positions)
+	for k, v := range r.Positions {
+		route.ID = r.ID
 		route.ClientID = r.ClientID
-		route.Position = []float64{v.Latitude, v.Longitude}
+		route.Position = []float64{v.Lat, v.Long}
+		route.Finished = false
 		if total-1 == k {
 			route.Finished = true
-		} else {
-			route.Finished = false
 		}
-
 		jsonRoute, err := json.Marshal(route)
 		if err != nil {
 			return nil, err
 		}
-
 		result = append(result, string(jsonRoute))
-
-		return result, nil
 	}
+	return result, nil
 }
