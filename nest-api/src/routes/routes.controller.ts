deb3a28ca94df9e57889ce459dd67ef5da0ad37e
Salmo Mascarenhas <salmo.cruz@gmail.com>
2023-02-10 17:00:25 -0300

    API part 1 finished.

--- nest-api/src/routes/routes.controller.ts
+++ nest-api/src/routes/routes.controller.ts
@@ -3,40 +3,84 @@ import {
   Controller,
   Delete,
   Get,
+  Inject,
+  OnModuleInit,
   Param,
   Patch,
   Post,
 } from '@nestjs/common'
+import { ClientKafka, MessagePattern, Payload } from '@nestjs/microservices'
+import { Producer } from '@nestjs/microservices/external/kafka.interface'
 import { CreateRouteDto } from './dto/create-route.dto'
 import { UpdateRouteDto } from './dto/update-route.dto'
 import { RoutesService } from './routes.service'
 
 @Controller('routes')
-export class RoutesController {
-  constructor(private readonly routesService: RoutesService) {}
+export class RoutesController implements OnModuleInit {
+  private kafkaProducer: Producer
+
+  constructor(
+    private readonly routesService: RoutesService,
+    @Inject('KAFKA_SERVICE')
+    private kafkaClient: ClientKafka,
+  ) { }
 
   @Post()
   create(@Body() createRouteDto: CreateRouteDto) {
-    return this.routesService.create(createRouteDto);
+    return this.routesService.create(createRouteDto)
   }
 
   @Get()
   findAll() {
-    return this.routesService.findAll();
+    return this.routesService.findAll()
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.routesService.findOne(+id);
+    return this.routesService.findOne(+id)
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateRouteDto: UpdateRouteDto) {
-    return this.routesService.update(+id, updateRouteDto);
+    return this.routesService.update(+id, updateRouteDto)
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.routesService.remove(+id);
+    return this.routesService.remove(+id)
+  }
+
+  async onModuleInit() {
+    this.kafkaProducer = await this.kafkaClient.connect()
+  }
+
+  @Get(':id/start')
+  startRoute(@Param('id') id: string) {
+    this.kafkaProducer.send({
+      topic: 'route.new-direction',
+      messages: [
+        {
+          key: 'route.new-direction',
+          value: JSON.stringify({
+            routeId: id,
+            clientId: '',
+          }),
+        },
+      ],
+    })
+  }
+
+  @MessagePattern('route.new-position')
+  consumeNewPosition(
+    @Payload() message: {
+      value: {
+        routeId: string,
+        clientId: string,
+        position: [number, number],
+        finished: boolean
+      }
+    },
+  ) {
+    console.log(message)
   }
 }
--- nest-api/src/routes/routes.module.ts
+++ nest-api/src/routes/routes.module.ts
@@ -1,9 +1,42 @@
-import { Module } from '@nestjs/common';
-import { RoutesService } from './routes.service';
-import { RoutesController } from './routes.controller';
+import { Module } from '@nestjs/common'
+import { ClientsModule, Transport } from '@nestjs/microservices'
+import { MongooseModule } from '@nestjs/mongoose'
+import { Route, RouteSchema } from './entities/route.entity'
+import { RoutesController } from './routes.controller'
+import { RoutesService } from './routes.service'
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Route.name, schema: RouteSchema }]),
+    ClientsModule.registerAsync([
+      {
+        name: 'KAFKA_SERVICE',
+        useFactory: (): any => ({
+          transport: Transport.KAFKA,
+          options: {
+            client: {
+              clientId: process.env.KAFKA_CLIENT_ID,
+              brokers: [process.env.KAFKA_BROKER],
+              // ssl: true,
+              // sasl: {
+              //   mechanism: 'plain', // scram-sha-256 or scram-sha-512
+              //   username: process.env.KAFKA_SASL_USERNAME,
+              //   password: process.env.KAFKA_SASL_PASSWORD,
+              // },
+            },
+            consumer: {
+              groupId:
+                !process.env.KAFKA_CONSUMER_GROUP_ID ||
+                  process.env.KAFKA_CONSUMER_GROUP_ID === ''
+                  ? 'my-consumer-' + Math.random()
+                  : process.env.KAFKA_CONSUMER_GROUP_ID,
+            },
+          },
+        }),
+      },
+    ]),
+  ],
   controllers: [RoutesController],
-  providers: [RoutesService]
+  providers: [RoutesService],
 })
-export class RoutesModule {}
+export class RoutesModule { }
