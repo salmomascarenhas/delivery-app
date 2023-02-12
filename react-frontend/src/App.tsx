e46c31d7ee9e54969206954bcae3690f20dfc50e
Salmo Mascarenhas <salmo.cruz@gmail.com>
2023-02-12 01:26:05 -0300

    Finish frontend.

--- react-frontend/src/App.tsx
+++ react-frontend/src/App.tsx
@@ -1,11 +1,16 @@
-import Mapping from "./components/Mapping"
-
+import { CssBaseline, MuiThemeProvider } from "@material-ui/core"
+import { SnackbarProvider } from "notistack"
+import { Mapping } from "./components/Mapping"
+import theme from "./theme"
 
 function App() {
   return (
-    <div className="App">
-      <Mapping />
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <SnackbarProvider maxSnack={3}>
+        <CssBaseline />
+        <Mapping />
+      </SnackbarProvider>
+    </MuiThemeProvider>
   )
 }
 
